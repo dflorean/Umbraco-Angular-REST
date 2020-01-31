a3ae7faa0187ec9f52910d7f6f126f067de6dab7
Daniele Florean <daniele.florean@ituna.it>
2020-01-31 16:11:17 +0100

    renamed external project

---
+++ Libraries/Umbraco.Extensions/Web/Mapping/HeadRestMapDefinition.cs
@@ -0,0 +1,16 @@
+﻿using iTuna.Umbraco.Extensions.Web.Mapping.Maps;
+using iTuna.Umbraco.Extensions.Web.ViewModels;
+using Umbraco.Core.Mapping;
+using Umbraco.Web.PublishedModels;
+
+namespace iTuna.Umbraco.Extensions.Web.Mapping
+{
+    public class HeadRestMapDefinition : IMapDefinition
+    {
+        public void DefineMaps(UmbracoMapper mapper)
+        {
+            mapper.Define<HomePage, HomePageViewModel>(HomePageViewModelMapping.Instance.Map);
+            mapper.Define<FirstLevelPage, FirstLevelPageViewModel>(FirstLevelPageViewModelMapping.Instance.Map);
+        }
+    }
+}
